ce7323ff6c15e176f703fe84e6cf84cc4753e59a
dv0c <tasosmeidanis12@gmail.com>
2024-09-01 23:47:54 +0300

    modified:   src/app/workspace/[workspaceId]/preferences-modal.tsx 	new file:   src/features/workspaces/api/use-remove-workspace.ts

--- src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -1,7 +1,12 @@
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { useRemoveWorkspace } from "@/features/workspaces/api/use-remove-workspace";
 import { useUpdateWorkspace } from "@/features/workspaces/api/use-update-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { TrashIcon } from "lucide-react";
 import { useState } from "react";
+import { toast } from "sonner";
 
 interface PreferencesModalProps {
     open: boolean;
@@ -10,9 +15,25 @@ interface PreferencesModalProps {
 }
 
 export const PreferencesModal = ({ initialValue, open, setOpen }: PreferencesModalProps) => {
+    const workspaceId = useWorkspaceId()
     const [value, setValue] = useState(initialValue)
+    const [editOpen, setEditOpen] = useState(false)
 
-    const {mutate: updateWorkspace, isPending: isUpdatingWorkspace} = useUpdateWorkspace()
+    const { mutate: updateWorkspace, isPending: isUpdatingWorkspace } = useUpdateWorkspace()
+    const { mutate: removeWorkspace, isPending: isRemovingWorkspace } = useRemoveWorkspace()
+
+    const handleEdit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        updateWorkspace({ name: value, id: workspaceId }, {
+            onSuccess() {
+                setEditOpen(false)
+                toast.success("Workspace updated")
+            },
+            onError: () => {
+                toast.error("Failed to update workspace")
+            }
+        })
+    }
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
@@ -23,25 +44,58 @@ export const PreferencesModal = ({ initialValue, open, setOpen }: PreferencesMod
                     </DialogTitle>
                 </DialogHeader>
                 <div className="px-4 pb-4 flex flex-col gap-y-2">
-                    <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
-                        <div className="flex items-center justify-between">
-                            <p className="text-sm font-semibold">
-                                Workspace name
-                            </p>
-                            <p className="text-sm text-[#1264a3] hover:underline font-semibold">Edit</p>
-                        </div>
-                        <p className="text-sm">{value}</p>
-                    </div>
+                    <Dialog open={editOpen} onOpenChange={setEditOpen}>
+                        <DialogTrigger asChild>
+                            <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                                <div className="flex items-center justify-between">
+                                    <p className="text-sm font-semibold">
+                                        Workspace name
+                                    </p>
+                                    <p className="text-sm text-[#1264a3] hover:underline font-semibold">Edit</p>
+                                </div>
+                                <p className="text-sm">{value}</p>
+                            </div>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>
+                                    Rename this workspace
+                                </DialogTitle>
+                            </DialogHeader>
+                            <form className="space-y-4" onSubmit={handleEdit}>
+                                <Input
+                                    value={value}
+                                    disabled={isUpdatingWorkspace}
+                                    onChange={(e) => setValue(e.target.value)}
+                                    required
+                                    autoFocus
+                                    minLength={3}
+                                    maxLength={80}
+                                    placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+                                />
+                                <DialogFooter>
+                                    <DialogClose asChild>
+                                        <Button variant={'outline'} disabled={isUpdatingWorkspace}>
+                                            Cancel
+                                        </Button>
+                                    </DialogClose>
+                                    <Button disabled={isUpdatingWorkspace}>
+                                        Save
+                                    </Button>
+                                </DialogFooter>
+                            </form>
+                        </DialogContent>
+                    </Dialog>
                     <button
-                    disabled={false}
-                    onClick={() => {}}
-                    className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 text-rose-600"
+                        disabled={false}
+                        onClick={() => { }}
+                        className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 text-rose-600"
                     >
                         <TrashIcon className="size-4" />
                         <p className="text-sm font-semibold">Delete workspace</p>
                     </button>
                 </div>
             </DialogContent>
-        </Dialog>
+        </Dialog >
     )
 }

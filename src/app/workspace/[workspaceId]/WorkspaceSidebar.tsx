a54c0b24e39c71320b98dabacb36bb98a30ee704
dv0c <tasosmeidanis12@gmail.com>
2024-09-01 19:09:40 +0300

    modified:   convex/members.ts 	modified:   package-lock.json 	modified:   package.json 	new file:   src/app/workspace/[workspaceId]/WorkspaceHeader.tsx 	modified:   src/app/workspace/[workspaceId]/WorkspaceSidebar.tsx 	new file:   src/components/ui/tooltip.tsx 	new file:   src/features/members/api/use-current-member.ts

--- src/app/workspace/[workspaceId]/WorkspaceSidebar.tsx
+++ src/app/workspace/[workspaceId]/WorkspaceSidebar.tsx
@@ -1,8 +1,37 @@
+'use client'
+
+import { useCurrentMember } from "@/features/members/api/use-current-member"
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace"
+import { useWorkspaceId } from "@/hooks/use-workspace-id"
+import { AlertTriangle, Loader } from "lucide-react"
+import { WorkspaceHeader } from "./WorkspaceHeader"
+
 export const WorkspaceSidebar = () => {
+    const workspaceId = useWorkspaceId()
+    const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId })
+    const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId })
+
+    if (workspaceLoading || memberLoading) {
+        return (
+            <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+                <Loader className="animate-spin size-5 text-white" />
+            </div>
+        )
+    }
+
+    if (!workspace || !member) {
+        return (
+            <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
+                <AlertTriangle className="size-5 text-white" />
+                <p className="text-white text-sm">Workspace not found</p>
+            </div>
+        )
+    }
+
 
     return (
-        <div>
-            hello
+        <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full ">
+            <WorkspaceHeader workspace={workspace} isAdmin={member.role === 'admin'} />
         </div>
     )
 }

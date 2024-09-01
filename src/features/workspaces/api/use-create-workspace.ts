e502eeb6b989e8ce698df2b3b5504f54faebf1bb
dv0c <tasosmeidanis12@gmail.com>
2024-09-01 17:01:49 +0300

    modified:   src/features/workspaces/api/use-create-workspace.ts 	modified:   src/features/workspaces/componens/create-workspace-modal.tsx

--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -1,13 +1,13 @@
 import { useMutation } from "convex/react";
 
-import { useCallback } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { api } from "../../../../convex/_generated/api";
 import { Id } from "../../../../convex/_generated/dataModel";
 
 type RequestType = {
   name: string;
 };
-type ResponseType = Id<"workspaces">;
+type ResponseType = Id<"workspaces"> | null;
 
 type Options = {
   onSuccess?: (data: ResponseType) => void;
@@ -17,13 +17,31 @@ type Options = {
 };
 
 export const useCreateWorkspace = () => {
-    
+  const [data, setData] = useState<ResponseType>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [status, setStatus] = useState<
+    "sucess" | "error" | "settled" | "pending" | null
+  >(null);
 
-    const mutation = useMutation(api.workspaces.create);
+  //   const [isPending, setIsPending] = useState(false);
+  //   const [isSuccess, setIsSuccess] = useState(false);
+  //   const [isError, setIsError] = useState(false);
+  //   const [isSettled, setIsSettled] = useState(false);
+
+  const isPending = useMemo(() => status === "pending", [status]);
+  const isSuccess = useMemo(() => status === "sucess", [status]);
+  const isError = useMemo(() => status === "error", [status]);
+  const isSettled = useMemo(() => status === "settled", [status]);
+
+  const mutation = useMutation(api.workspaces.create);
 
   const mutate = useCallback(
     async (values: RequestType, options?: Options) => {
       try {
+        setData(null);
+        setError(null);
+        setStatus("pending");
+
         const response = await mutation(values);
         options?.onSuccess?.(response);
         return response;
@@ -33,6 +51,7 @@ export const useCreateWorkspace = () => {
           throw error;
         }
       } finally {
+        setStatus("settled");
         options?.onSettled?.();
       }
     },
@@ -41,5 +60,11 @@ export const useCreateWorkspace = () => {
 
   return {
     mutate,
+    data,
+    isError,
+    error,
+    isSuccess,
+    isPending,
+    isSettled,
   };
 };
--- src/features/workspaces/componens/create-workspace-modal.tsx
+++ src/features/workspaces/componens/create-workspace-modal.tsx
@@ -1,35 +1,32 @@
 'use client'
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useCreateWorkspace } from "../api/use-create-workspace";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useCreateWorkspace } from "../api/use-create-workspace";
+import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
 
 export const CreateWorkspaceModal = () => {
     const router = useRouter()
     const [open, setOpen] = useCreateWorkspaceModal();
+    const [name, setName] = useState("");
 
-    const { mutate } = useCreateWorkspace();
+    const { mutate, isPending } = useCreateWorkspace();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        mutate({ name }, {
+            onSuccess(data) {
+                console.log(data)
+            }
+        })
+    }
 
     const handleClose = () => {
         setOpen(false)
     }
 
-    const handleSubmit = async () => {
-        try {
-
-            const data = await mutate({
-                name: "Workspace 1",
-            }, {
-                onSuccess(data) {
-                    router.push(`/workspaces/${data}`)
-                }
-            })
-        } catch (error) {
-            console.error(error)
-        }
-    }
 
     return (
         <Dialog onOpenChange={handleClose} open={open}>
@@ -37,10 +34,17 @@ export const CreateWorkspaceModal = () => {
                 <DialogHeader>
                     <DialogTitle>Add a workspace</DialogTitle>
                 </DialogHeader>
-                <form className="space-y-4">
-                    <Input disabled={false} value={""} required autoFocus minLength={3} placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'" />
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <Input
+                        onChange={(e) => setName(e.target.value)}
+                        disabled={isPending}
+                        value={name}
+                        required
+                        autoFocus
+                        minLength={3}
+                        placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'" />
                     <div className="flex justify-end">
-                        <Button disabled={false}>
+                        <Button disabled={isPending}>
                             Create
                         </Button>
                     </div>
